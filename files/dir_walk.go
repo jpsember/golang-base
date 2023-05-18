9dc8983afe69debca3633cdeb1d5fc504176863d
Jeff Sember <jpsember@gmail.com>
2023-05-18 09:34:28 -0700

    Issue #3: added Set class

--- files/dir_walk.go
+++ files/dir_walk.go
@@ -9,12 +9,14 @@ import (
 
 type DirWalk struct {
 	withRecurse    bool
-	patternsToOmit map[*regexp.Regexp]bool
+	patternsSet    *Set[string]
+	patternsToOmit []*regexp.Regexp
 }
 
 func NewDirWalk() *DirWalk {
 	var w = new(DirWalk)
-	w.patternsToOmit = make(map[*regexp.Regexp]bool)
+	w.patternsToOmit = []*regexp.Regexp{}
+	w.patternsSet = NewSet[string]()
 	return w
 }
 
@@ -25,9 +27,13 @@ func (w *DirWalk) WithRecurse(flag bool) *DirWalk {
 
 func (w *DirWalk) OmitNames(nameExprs ...string) *DirWalk {
 	for _, expr := range nameExprs {
+		if !w.patternsSet.Add(expr) {
+			continue
+		}
 		r, err := regexp.Compile(expr)
 		CheckOk(err, "failed to compile omit expr:", expr)
-		w.patternsToOmit[r] = true
+		Todo("Add an ArrayList class")
+		w.patternsToOmit = append(w.patternsToOmit, r)
 	}
 	return w
 }
---
+++ files/set.go
@@ -0,0 +1,30 @@
+package files
+
+import (
+	. "github.com/jpsember/golang-base/base"
+)
+
+var _ = Pr
+
+type Set[KEY comparable] struct {
+	wrappedMap map[KEY]bool
+}
+
+func NewSet[KEY comparable]() *Set[KEY] {
+	m := new(Set[KEY])
+	m.wrappedMap = make(map[KEY]bool)
+	return m
+}
+
+func (set *Set[KEY]) Add(value KEY) bool {
+	found := set.Contains(value)
+	if !found {
+		set.wrappedMap[value] = true
+	}
+	return found
+}
+
+func (set *Set[KEY]) Contains(value KEY) bool {
+	_, found := set.wrappedMap[value]
+	return found
+}
