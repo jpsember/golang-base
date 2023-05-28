5cd6187b06782b1abfcbf5ffad84df0d2e8f7e22
Jeff Sember <jpsember@gmail.com>
2023-05-28 12:12:44 -0700

    Issue #13: validated it with validator.w3.org

--- webserv/web_server.go
+++ webserv/web_server.go
@@ -82,22 +82,16 @@ func (oper *SampleOper) handle(w http.ResponseWriter, req *http.Request) {
 	sb := NewBasePrinter()
 
 	sb.Pr(`
-<HMTL>
-
-<HEAD>
-<TITLE>Example</TITLE>
-
-<script
-  src="https://code.jquery.com/jquery-1.12.4.min.js"
-  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-  crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Example</title>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-</HEAD>
-
-<BODY>
+</head>
+<body>
 `)
 
 	sb.Pr("Request received at:", time.Now().Format(time.ANSIC), CR)
@@ -115,13 +109,13 @@ func (oper *SampleOper) handle(w http.ResponseWriter, req *http.Request) {
 	sb.Pr(`<p>Click on the "Choose File" button to upload a file:</p>
 
 <form action="upload" enctype="multipart/form-data" method="post">
-    <input type="file" name="file" id="file" />
-    <input type="submit" />
+    <input type="file" name="file" id="file">
+    <input type="submit">
 </form>
 
 `)
 	sb.Pr(`
-</BODY>
+</body></html>
 `)
 
 	w.Header().Set("Content-Type", "text/html")
