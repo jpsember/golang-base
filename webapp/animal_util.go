beae0d8a9a926b1056810dc6de090ec00d706a44
Jeff Sember <jpsember@gmail.com>
2023-08-05 12:23:30 -0700

    Issue #42: adding random animal

--- webapp/animal_util.go
+++ webapp/animal_util.go
@@ -6,11 +6,9 @@ import (
 	"github.com/jpsember/golang-base/webserv"
 )
 
-func RandomAnimal() webapp_data.Animal {
+func RandomAnimal() webapp_data.AnimalBuilder {
 	r := webserv.HTMLRand.Rand()
 	a := webapp_data.NewAnimal()
-	a.SetId(r.Int63n(50000) + 1)
-	Pr("random animal, set id to:", a.Id())
 	a.SetName(RandomText(r, 20, false))
 	a.SetSummary(RandomText(r, 300, false))
 	a.SetDetails(RandomText(r, 2000, true))
@@ -20,5 +18,10 @@ func RandomAnimal() webapp_data.Animal {
 }
 
 func GenerateRandomAnimals() {
-
+	for i := 0; i < 100; i++ {
+		anim := RandomAnimal()
+		db := Db()
+		db.AddAnimal(anim)
+		Pr("added animal:", INDENT, anim)
+	}
 }
--- webapp/database.go
+++ webapp/database.go
@@ -7,8 +7,8 @@ package webapp
 import (
 	"database/sql"
 	. "github.com/jpsember/golang-base/base"
+	"github.com/jpsember/golang-base/webapp/gen/webapp_data"
 	_ "github.com/mattn/go-sqlite3"
-	"math/rand"
 )
 
 type DatabaseStruct struct {
@@ -54,8 +54,9 @@ func (db Database) SetDataSourceName(dataSourceName string) {
 func (db Database) Open() {
 	CheckState(db.state == dbStateNew, "Illegal state:", db.state)
 	CheckState(db.dataSourceName != "", "<1No call to SetDataSourceName made")
-	db.db, db.err = sql.Open("sqlite3", db.dataSourceName)
-	if db.ErrorOccurred() {
+	database, err := sql.Open("sqlite3", db.dataSourceName)
+	db.db = database
+	if db.SetError(err) {
 		db.state = dbStateFailed
 		return
 	}
@@ -70,26 +71,28 @@ func (db Database) Close() {
 	}
 }
 
-func (d Database) SetError(e error) {
+func (d Database) SetError(e error) bool {
 	d.err = e
-	if e != nil {
+	if d.HasError() {
 		Alert("<1#50Setting database error:", INDENT, e)
 	}
+	return d.HasError()
+}
+
+func (d Database) HasError() bool {
+	return d.err != nil
+}
+
+func (d Database) ClearError() Database {
+	d.err = nil
+	return d
 }
 
 func (db Database) AssertOk() Database {
-	if db.err != nil {
+	if db.HasError() {
 		BadState("<1DatabaseSqlite has an error:", db.err)
 	}
 	return db
-}
-
-func (db Database) ErrorOccurred() bool {
-	if db.err != nil {
-		Pr("*** Database error occurred:", INDENT, db.err)
-		return true
-	}
-	return false
 }
 
 func SQLiteExperiment() {
@@ -103,64 +106,93 @@ func SQLiteExperiment() {
 
 	Pr("opened db")
 
-	// Apparently it creates a database if none exists...?
+	a := RandomAnimal()
+	d.AddAnimal(a)
+	d.AssertOk()
+	Pr("added animal:", INDENT, a)
 
-	// Create a table if it doesn't exist
-	const create string = `
-  CREATE TABLE IF NOT EXISTS zebra (
-  uid INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(64) NOT NULL,
-  age INTEGER
-  );`
-
-	db := d.db
-
-	CheckOkWith(db.Exec(create))
-
-	rows := CheckOkWith(db.Query("SELECT * FROM user"))
-
-	rowTotal := 0
-	for rows.Next() {
-		rowTotal++
-		var uid int
-		var name string
-		var age int
-		CheckOk(rows.Scan(&uid, &name, &age))
-		Pr("uid:", uid, "name:", name, "age:", age)
-	}
-
-	// I assume this prepares an SQL statement (doing the optimization to determine best way to fulfill the statement)
-	addUserStatement := CheckOkWith(db.Prepare("INSERT INTO user(name, age) values(?,?)"))
-
-	// If it's empty, create a user
-	if rowTotal == 0 {
-		res := CheckOkWith(addUserStatement.Exec("Fred", 42))
-		affected, _ := res.RowsAffected()
-		Pr("affected rows:", affected)
-	}
-
-	rnd := rand.New(rand.NewSource(1965))
-	for i := 0; i < 100-rowTotal; i++ {
-		name := RandomText(rnd, 20, false)
-		age := rnd.Intn(65) + 8
-		CheckOkWith(addUserStatement.Exec(name, age))
-	}
+	//// Apparently it creates a database if none exists...?
+	//
+	//// Create a table if it doesn't exist
+	//const create string = `
+	//CREATE TABLE IF NOT EXISTS zebra (
+	//uid INTEGER PRIMARY KEY AUTOINCREMENT,
+	//name VARCHAR(64) NOT NULL,
+	//age INTEGER
+	//);`
+	//
+	//db := d.db
+	//
+	//CheckOkWith(db.Exec(create))
+	//
+	//rows := CheckOkWith(db.Query("SELECT * FROM user"))
+	//
+	//rowTotal := 0
+	//for rows.Next() {
+	//	rowTotal++
+	//	var uid int
+	//	var name string
+	//	var age int
+	//	CheckOk(rows.Scan(&uid, &name, &age))
+	//	Pr("uid:", uid, "name:", name, "age:", age)
+	//}
+	//
+	//// I assume this prepares an SQL statement (doing the optimization to determine best way to fulfill the statement)
+	//addUserStatement := CheckOkWith(db.Prepare("INSERT INTO user(name, age) values(?,?)"))
+	//
+	//// If it's empty, create a user
+	//if rowTotal == 0 {
+	//	res := CheckOkWith(addUserStatement.Exec("Fred", 42))
+	//	affected, _ := res.RowsAffected()
+	//	Pr("affected rows:", affected)
+	//}
+	//
+	//rnd := rand.New(rand.NewSource(1965))
+	//for i := 0; i < 100-rowTotal; i++ {
+	//	name := RandomText(rnd, 20, false)
+	//	age := rnd.Intn(65) + 8
+	//	CheckOkWith(addUserStatement.Exec(name, age))
+	//}
 
 }
 
 func (d Database) CreateTables() {
 	db := d.db
-	// Create a table if it doesn't exist
-	const create string = `
+
+	if false {
+		const drop = `DROP TABLE IF EXISTS animal;`
+		_, err := db.Exec(drop)
+		d.SetError(err)
+		d.AssertOk()
+	}
+
+	{
+		// Create a table if it doesn't exist
+		const create string = `
  CREATE TABLE IF NOT EXISTS animal (
      uid INTEGER PRIMARY KEY AUTOINCREMENT,
      name VARCHAR(64) NOT NULL,
      summary VARCHAR(300),
      details VARCHAR(3000),
      campaign_target INT,
-     campain_balance INT 
+     campaign_balance INT 
      );`
-	_, err := db.Exec(create)
-	d.SetError(err)
-	d.AssertOk()
+		_, err := db.Exec(create)
+		d.SetError(err)
+		d.AssertOk()
+	}
+}
+
+func (d Database) AddAnimal(a webapp_data.AnimalBuilder) {
+	d.ClearError()
+
+	result, err := d.db.Exec(`INSERT INTO animal (name, summary, details, campaign_target, campaign_balance) VALUES(?,?,?,?,?)`,
+		a.Name(), a.Summary(), a.Details(), a.CampaignTarget(), a.CampaignBalance())
+	if !d.SetError(err) {
+		id, err2 := result.LastInsertId()
+		if !d.SetError(err2) {
+			a.SetId(id)
+		}
+	}
+
 }
