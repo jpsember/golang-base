2665937a28ecd267df60d79165f6478ee90666b5
Jeff Sember <jpsember@gmail.com>
2023-08-22 10:36:24 -0700

    Issue #51: making scratch copy of omitted files during merge

--- merge.sh
+++ merge.sh
@@ -8,14 +8,18 @@ set -eu
 # The untouched files are at present the single file "webapp/database.go"
 #
 
+OMITTED_FILE="webapp/database.go"
+
 MAIN_BRANCH="main"
 ALT_BRANCH="no-db"
 
 CURRENT_BRANCH=`git branch --show-current`
 if [ "$CURRENT_BRANCH" == "$MAIN_BRANCH" ]; then
   OTHER_BRANCH=$ALT_BRANCH
+  cp $OMITTED_FILE webapp/_SKIP_database_main.go
 elif [ "$CURRENT_BRANCH" == "$ALT_BRANCH" ]; then
   OTHER_BRANCH=$MAIN_BRANCH
+  cp $OMITTED_FILE webapp/_SKIP_database_no-db.go
 else
   echo "Current branch is $CURRENT_BRANCH, expected either $MAIN_BRANCH or $ALT_BRANCH !!!"
   exit 1
@@ -27,7 +31,6 @@ echo
 echo "See: https://stackoverflow.com/questions/15232000/git-ignore-files-during-merge"
 echo
 
-OMITTED_FILE="webapp/database.go"
 echo "Omitting: $OMITTED_FILE"
 
 git merge --no-ff --no-commit $OTHER_BRANCH
