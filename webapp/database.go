f14516a5ff8674ad46a7ee620d24296aec44a139
Jeff Sember <jpsember@gmail.com>
2023-08-27 08:26:02 -0700

    Issue #53: stripping some stuff out of Database

--- webapp/database.go
+++ webapp/database.go
@@ -14,12 +14,9 @@ import (
 
 type DatabaseStruct struct {
 	state          int
-	err            error
 	dataSourceName Path
 	sqlDatabase    *sql.DB
 	theLock        sync.Mutex
-	blobLock       sync.Mutex
-	userLock       sync.Mutex
 }
 
 type Database = *DatabaseStruct
@@ -44,6 +41,7 @@ func CreateDatabase() Database {
 	return Db()
 }
 
+// This function probably doesn't need to be called much anymore, which is nice.
 func Db() Database {
 	CheckState(singletonDatabase != nil, "<1No database created yet")
 	return singletonDatabase
@@ -52,7 +50,6 @@ func Db() Database {
 func (db Database) SetDataSourceName(dataSourceName Path) {
 	CheckState(db.state == dbStateNew, "Illegal state:", db.state)
 	db.dataSourceName = dataSourceName
-	//Alert("<1Setting data source name:", dataSourceName, CurrentDirectory())
 }
 
 func (db Database) Open() error {
@@ -65,77 +62,64 @@ func (db Database) Open() error {
 
 	database, err := sql.Open("sqlite3", db.dataSourceName.String())
 	db.sqlDatabase = database
-	if db.setError(err) {
+	if err != nil {
 		db.state = dbStateFailed
 	} else {
 		db.state = dbStateOpen
 		PrepareDatabase(db.sqlDatabase, &db.theLock)
 	}
 
-	if Alert("experiment with blob") {
-		var dat = []byte{1, 2, 3, 4}
+	if err != nil {
+		if Alert("experiment with blob") {
+			var dat = []byte{1, 2, 3, 4}
 
-		bb := NewBlob()
-		bb.SetData(dat)
+			bb := NewBlob()
+			bb.SetData(dat)
 
-		key1 := string(GenerateBlobId())
+			key1 := string(GenerateBlobId())
 
-		bb.SetName(key1)
-		result, err := CreateBlobWithName(bb)
-		CheckOk(err)
+			bb.SetName(key1)
+			result, err := CreateBlobWithName(bb)
+			CheckOk(err)
 
-		bb = result.ToBuilder()
-		result2, err2 := CreateBlobWithName(bb)
-		Pr("attempt to create duplicate blob:", result2, "err:", err2)
+			bb = result.ToBuilder()
+			result2, err2 := CreateBlobWithName(bb)
+			Pr("attempt to create duplicate blob:", result2, "err:", err2)
+		}
+		if !Alert("experiment") {
+			u := NewUser().SetEmail("a").SetPassword("pasword").SetState(UserstateActive).SetName("jeff")
+			Pr("attempting to read user with id 1")
+			uf, errf :=
+				ReadUser(1)
+			Pr("found user:", uf, "err:", errf)
+
+			Pr("attempting to create user:", INDENT, u)
+			u2, err := CreateUser(u)
+			CheckOk(err)
+			Pr("created:", INDENT, u2)
+			u3 := u2.ToBuilder().SetName("Frank")
+			err2 := UpdateUser(u3)
+			Pr("updated:", INDENT, u3, "err:", err2)
+
+			Pr("attempting to find user 1 now that it exists")
+			uf, errf =
+				ReadUser(1)
+			Pr("found user:", uf, "err:", errf)
+		}
 	}
-	if !Alert("experiment") {
-		u := NewUser().SetEmail("a").SetPassword("pasword").SetState(UserstateActive).SetName("jeff")
-		Pr("attempting to read user with id 1")
-		uf, errf :=
-			ReadUser(1)
-		Pr("found user:", uf, "err:", errf)
 
-		Pr("attempting to create user:", INDENT, u)
-		u2, err := CreateUser(u)
-		CheckOk(err)
-		Pr("created:", INDENT, u2)
-		u3 := u2.ToBuilder().SetName("Frank")
-		err2 := UpdateUser(u3)
-		Pr("updated:", INDENT, u3, "err:", err2)
-
-		Pr("attempting to find user 1 now that it exists")
-		uf, errf =
-			ReadUser(1)
-		Pr("found user:", uf, "err:", errf)
-	}
-
-	return db.err
+	return err
 }
 
 func (db Database) Close() error {
+	var err error
 	if db.state == dbStateOpen {
 		db.Lock()
 		defer db.Unlock()
-		db.setError(db.sqlDatabase.Close())
+		err = db.sqlDatabase.Close()
 		db.state = dbStateClosed
 	}
-	return db.err
-}
-
-// If no registered error exists, set it.  Return true if registered error exists afterwards.
-func (db Database) setError(err error) bool {
-	if err != nil {
-		if db.err == nil {
-			db.err = err
-			Alert("<1#50Setting database error:", INDENT, err)
-		}
-		return true
-	}
-	return false
-}
-
-func (db Database) ok() bool {
-	return db.err == nil
+	return err
 }
 
 func (db Database) DeleteAllRowsInTable(name string) error {
@@ -143,8 +127,7 @@ func (db Database) DeleteAllRowsInTable(name string) error {
 	defer db.Unlock()
 	database := db.sqlDatabase
 	_, err := database.Exec(`DELETE FROM ` + name)
-	db.setError(err)
-	return db.err
+	return err
 }
 
 // Acquire the lock on the database, and clear the error register.
@@ -153,7 +136,6 @@ func (db Database) Lock() {
 		BadState("<1Illegal state:", db.state)
 	}
 	db.theLock.Lock()
-	db.err = nil
 }
 
 func (db Database) Unlock() {
