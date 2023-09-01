8141f0e27b2b3ea3202786ccf0488e22dc97c47a
Jeff Sember <jpsember@gmail.com>
2023-09-01 15:44:51 -0700

    Issue #53

--- dgen.sh
+++ dgen.sh
@@ -2,9 +2,9 @@
 set -eu
 
 datagen language go format source_path gen clean "$@"
+
 datagen --exceptions language go \
    format \
    source_path webapp/gen \
    dat_path webapp/dat_files \
-   dbsim \
    clean "$@"
